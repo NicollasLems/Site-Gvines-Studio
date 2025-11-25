9dde481d544632fb90b66c5ab8f0309b07a94cdf
NicollasLems <nicollas.matos@etec.sp.gov.br>
2025-11-25 18:26:22 -0300

    Criação e adição de arquivos referentes ao Site da empresa Gvines Studio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function saibaMais() {
+  alert("Mais informações em breve!");
+}
+
+/*function scrollCarousel(direction) {
+  const track = document.querySelector('.carousel-track');
+  const scrollAmount = 150;
+  track.scrollBy({ left: scrollAmount * direction, behavior: 'smooth' });
+}*/
